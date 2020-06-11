3529cea7a730b1d4e00ebeec50a3e81cbe12a3ee
bonald <bonald@gmail.com>
2020-06-11 13:14:00 -0300

    OKEX

--- cryptofeed/exchange/okex.py
+++ cryptofeed/exchange/okex.py
@@ -4,7 +4,7 @@ Copyright (C) 2017-2020  Bryant Moscon - bmoscon@gmail.com
 Please see the LICENSE file for the terms and conditions
 associated with this software.
 '''
-from cryptofeed.defines import OKEX
+from cryptofeed.defines import OKEX, OKEX_SWAP, OKEX_FUTURES
 from cryptofeed.exchange.okcoin import OKCoin
 
 import requests

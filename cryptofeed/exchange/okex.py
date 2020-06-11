318fd260f06689368c866ea62dbe0f2d1742f85a
bonald <bonald@gmail.com>
2020-06-11 13:26:26 -0300

    OKEX.PY

--- cryptofeed/exchange/okex.py
+++ cryptofeed/exchange/okex.py
@@ -32,3 +32,49 @@ class OKEx(OKCoin):
         for data in OKEx.get_active_symbols_info():
             symbols.append(data['instrument_id'])
         return symbols
+    
+class OKEx_swap(OKCoin):
+    """
+    OKEx has the same api as OKCoin, just a different websocket endpoint
+    """
+    id = OKEX_SWAP
+    api = 'https://www.okex.com/api/'
+
+    def __init__(self, pairs=None, channels=None, callbacks=None, **kwargs):
+        super().__init__(pairs=pairs, channels=channels, callbacks=callbacks, **kwargs)
+        self.address = 'wss://real.okex.com:8443/ws/v3'
+        self.book_depth = 200
+
+    @staticmethod
+    def get_active_symbols_info():
+        return requests.get(OKEx.api + 'futures/v3/instruments').json()
+
+    @staticmethod
+    def get_active_symbols():
+        symbols = []
+        for data in OKEx.get_active_symbols_info():
+            symbols.append(data['instrument_id'])
+        return symbols
+    
+class OKEx_futures(OKCoin):
+    """
+    OKEx has the same api as OKCoin, just a different websocket endpoint
+    """
+    id = OKEX_FUTURES
+    api = 'https://www.okex.com/api/'
+
+    def __init__(self, pairs=None, channels=None, callbacks=None, **kwargs):
+        super().__init__(pairs=pairs, channels=channels, callbacks=callbacks, **kwargs)
+        self.address = 'wss://real.okex.com:8443/ws/v3'
+        self.book_depth = 200
+
+    @staticmethod
+    def get_active_symbols_info():
+        return requests.get(OKEx.api + 'futures/v3/instruments').json()
+
+    @staticmethod
+    def get_active_symbols():
+        symbols = []
+        for data in OKEx.get_active_symbols_info():
+            symbols.append(data['instrument_id'])
+        return symbols
